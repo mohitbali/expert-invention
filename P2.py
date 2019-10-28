7c8db7f21dbfa72b57593b7e2d8bf9c0e966aeee
mohitbali <35188312+mohitbali@users.noreply.github.com>
2019-10-28 22:13:18 +0530

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,10 @@
+# If-else Program
+
+user_input = int(input('Enter a Number: '))
+
+if(user_input < 100):
+    print('Number entered is less than 100')
+elif(user_input == 100):
+    print('Number entered is 100')
+else:
+    print('Number entered is greater than 100')
